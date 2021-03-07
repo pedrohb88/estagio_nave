c967c5179b10304fba7604caf3ac1dcc910c1665
Pedro Leal <pedrohb88@hotmail.com>
2021-03-06 23:23:33 -0300

    database ssl

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,7 @@ module.exports = {
 			user: process.env.PG_USER,
 			password: process.env.PG_PASSWORD,
 			database: process.env.PG_DATABASE,
+			ssl: true,
 		},
 		migrations: {
 			tableName: 'knex_migrations',
